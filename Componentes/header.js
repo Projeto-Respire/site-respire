f807927d7e5250460db21a7a3172782571f53b0b
lleolel <koy.skullz@gmail.com>
2025-11-22 00:26:35 -0300

    att

--- Componentes/header.js
+++ Componentes/header.js
@@ -2,7 +2,7 @@ export function Header() {
   return ` 
   <header id="header">
     <div class="tipografia">
-      <img src="/Assets/imagens/RESPIRE.png" alt="respire-logo-tipografia">
+      <img src="/Assets/imagens/respire-tipografia.png" alt="respire-logo-tipografia">
     </div>
   <nav id="nav">
     <button aria-label="Abrir Menu" id="mobo-btn" aria-haspopup="true" aria-controls="menu" aria-expanded="false">Menu
@@ -10,12 +10,10 @@ export function Header() {
     </button>
     <ul id="menu" role="menu">
       <li><a href="/index.html">Home</a></li>
-      <li><a href="/index.html>Sobre</a></li>
+      <li><a href="/sobre.html">Quem somos</a></li>
       <li><a href="/pages/sintomas.html">Sintomas</a></li>
       <li><a href="/pages/dicas.html">Dicas</a></li>
       <li><a href="/pages/noticias.html">Notícias</a></li>
-      <li><a href="/pages/voluntarios.html">Voluntarios</a></li>
-      <li><a href="/pages/comunidades.html">Comunidades</a></li>
     </ul>
   </nav>
 <header>
